da0f0bd36b930ec608c6a755911c78a838f2536f
Santiago Soria <lsoriac@est.ups.edu.ec>
2021-05-07 09:57:56 -0500

    Añadir Campo email

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 RegisterControl.createUser = async(req, res) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.URL_FRONT);
     try {
-        const { nombre, contrasena, usuario } = req.body
+        const { nombre, contrasena, usuario, email } = req.body
             //encrypt
         contra = bcrypt.hashSync(contrasena, 10)
         var values = [
-                [nombre, contra, usuario]
+                [nombre, contra, usuario, email]
             ]
             //query database
-        await pool.query("INSERT INTO usuarios (nombre, contrasena, usuario) VALUES ?", [values], (error, result) => {
+        await pool.query("INSERT INTO usuarios (nombre, contrasena, usuario, email) VALUES ?", [values], (error, result) => {
             res.json({
                 status: true,
                 message: "Usuario Registrado de forma correcta",
@@ -29,7 +29,6 @@ RegisterControl.createUser = async(req, res) => {
     }
     //close connnection
     pool.end()
-
 }
 
 
