cdfeaf2364906b5217402bad59701764cb8223f3
Santiago Soria <lsoriac@est.ups.edu.ec>
2021-05-16 12:28:11 -0500

    Actualizacion seguridad enviar email

--- src/controllers/recover.js
+++ src/controllers/recover.js
@@ -14,8 +14,11 @@ RecoverControl.updatePass = async(req, res) => {
             numbers: true,
             symbols: true
         });
-        //find email and change password
-        await pool.query("UPDATE `usuarios` SET `contrasena` =" + "'" + password + "'" + " WHERE (`email` = '" + email + "')", (error, userDB) => {
+        //console.log(password);
+        //encrypt
+        let pass_encrypt = bcrypt.hashSync(password, 10)
+            //find email and change password
+        await pool.query("UPDATE `usuarios` SET `contrasena` =" + "'" + pass_encrypt + "'" + " WHERE (`email` = '" + email + "')", (error, userDB) => {
             //error sql
             if (error) {
                 return res.status(400).json({
@@ -34,14 +37,37 @@ RecoverControl.updatePass = async(req, res) => {
                 });
             }
 
+            //Content email message
+            contentHTML = `
+        <h1>Para ingresar a su cuenta use la siguiente contraseña</h1>
+       
+        <p>${password}</p>
+            `;
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // use SSL
+                service: 'Gmail',
+                // true for 465, false for other ports
+                auth: {
+                    user: process.env.USER_EMAIL,
+                    pass: process.env.PASS_EMAIL
+                }
+            });
+            transporter.sendMail({
+                from: '"Recuperar Contraseña Fenix Ventas" <lenisantiagos@gmail.com>', // sender address
+                to: email, // list of receivers
+                subject: "Recuperación de Contraseña",
+                html: contentHTML, // html body
+            })
+
+
             res.json({
                 status: true,
-                message: "Contraseña modificada",
-                param: password
-                    //usuario: userDB //check info to send
+                message: "Modificación exitosa",
             })
         })
-
     } catch (error) {
         res.status(400).json({
             status: false,
@@ -51,48 +77,6 @@ RecoverControl.updatePass = async(req, res) => {
     }
     //close connnection
     pool.end()
-}
-
-RecoverControl.sendEmail = async(req, res) => {
-    const { param, email } = req.body;
-    //Content email message
-    contentHTML = `
- <h1>Para ingresar a su cuenta use la siguiente contraseña</h1>
-
- <p>${param}</p>
-     `;
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // use SSL
-        service: 'Gmail',
-        // true for 465, false for other ports
-        auth: {
-            user: process.env.USER_EMAIL,
-            pass: process.env.PASS_EMAIL
-        }
-    });
-    try {
-        const sendemail = await transporter.sendMail({
-            from: '"Recuperar Contraseña Fenix Ventas" <lenisantiagos@gmail.com>', // sender address
-            to: email, // list of receivers
-            subject: "Recuperación de Contraseña", // Subject line
-            //text: "Hello world??", // plain text body
-            html: contentHTML, // html body
-        })
-        res.json({
-            status: true,
-            message: "Correo enviado",
-        })
-
-    } catch (error) {
-        res.status(400).json({
-            status: false,
-            message: 'Error al menviar el correo',
-            error
-        })
-    }
 }
 
 module.exports = RecoverControl
--- src/routes/recover.js
+++ src/routes/recover.js
@@ -1,9 +1,8 @@
 //Import modules
 const express = require('express')
 const router = express.Router();
-const { updatePass, sendEmail } = require('../controllers/recover');
+const { updatePass } = require('../controllers/recover');
 router.route('/')
     .put(updatePass)
-    .post(sendEmail)
 
 module.exports = router;
